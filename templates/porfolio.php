836077a7e439d9b2254aa394ffd0b4981c2034ae
Matt McLean <9052437+doctor-spaceman@users.noreply.github.com>
2020-11-22 18:14:54 -0500

    Add folder for custom page templates

---
+++ templates/porfolio.php
@@ -0,0 +1,20 @@
+<?php 
+/* Template Name: Portfolio */ 
+
+?>
+
+<?php get_header(); ?>
+
+<section class="wrapper">
+<?php 
+if ( have_posts() ) : 
+  while ( have_posts() ) : 
+    the_post();
+    the_content();
+  endwhile; 
+endif; 
+?>
+
+</section>
+
+<?php get_footer(); ?>
