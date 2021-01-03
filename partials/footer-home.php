8ca7030219d186dae92dc8e34b0bbd4f21432197
Matt McLean <9052437+doctor-spaceman@users.noreply.github.com>
2021-01-03 15:07:14 -0500

    Add attribution to home footer

--- partials/footer-home.php
+++ partials/footer-home.php
@@ -1,7 +1,9 @@
 <footer class="sticky-footer">
-  <div class="wrapper footer">
+  <div class="wrapper wrapper--large grid grid--center grid--space footer">
+    <?php if ( $footerElements['footer_attribution'] ) : ?>
     <div class="footer__attribution">
-      <?php dynamic_sidebar('footer-attribution'); ?>
+      <?php echo $footerElements['footer_attribution']; ?>
     </div>
+    <?php endif; ?>
   </div>
 </footer>
