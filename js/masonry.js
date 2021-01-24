54d35238d52d55498ffdf20a4ce0731734a9aa9d
Matt McLean <9052437+doctor-spaceman@users.noreply.github.com>
2021-01-24 13:56:37 -0500

    Fix issue with masonry grid items overlapping on page load

--- js/masonry.js
+++ js/masonry.js
@@ -2475,14 +2475,13 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
 
 jQuery(function () {
   var portfolio_sidebar_grid = jQuery('.page-template-portfolio-sidebar.page-child .masonry');
-  portfolio_sidebar_grid.masonry({
-    columnWidth: '.grid-sizer',
-    gutter: 16,
-    itemSelector: '.grid-item',
-    percentPosition: true
-  });
   portfolio_sidebar_grid.imagesLoaded(function () {
-    portfolio_sidebar_grid.masonry('layout');
+    portfolio_sidebar_grid.masonry({
+      columnWidth: '.grid-sizer',
+      gutter: 16,
+      itemSelector: '.grid-item',
+      percentPosition: true
+    });
     portfolio_sidebar_grid.addClass('loaded');
   });
   jQuery('.masonry img').on('click', function () {
--- js/masonry/masonry-init.js
+++ js/masonry/masonry-init.js
@@ -1,15 +1,13 @@
 jQuery(function() {
   var portfolio_sidebar_grid = jQuery('.page-template-portfolio-sidebar.page-child .masonry');
 
-  portfolio_sidebar_grid.masonry({
-    columnWidth: '.grid-sizer',
-    gutter: 16,
-    itemSelector: '.grid-item',
-    percentPosition: true
-  });
-
   portfolio_sidebar_grid.imagesLoaded(function() { 
-    portfolio_sidebar_grid.masonry('layout');
+    portfolio_sidebar_grid.masonry({
+      columnWidth: '.grid-sizer',
+      gutter: 16,
+      itemSelector: '.grid-item',
+      percentPosition: true
+    });
     portfolio_sidebar_grid.addClass('loaded');
   });
 
