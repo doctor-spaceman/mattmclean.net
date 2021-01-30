82da49bdfeb132e7c1c9ae9a1477e4738b1491ae
Matt McLean <9052437+doctor-spaceman@users.noreply.github.com>
2021-01-30 17:40:03 -0500

    Change load method for masonry template

--- js/masonry.js
+++ js/masonry.js
@@ -2475,14 +2475,13 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
 
 jQuery(function () {
   var $portfolio_sidebar_grid = jQuery('.page-template-portfolio-sidebar.page-child .masonry');
-  var grid = $portfolio_sidebar_grid.masonry({
-    columnWidth: '.grid-sizer',
-    gutter: 16,
-    itemSelector: '.grid-item',
-    percentPosition: true
-  });
-  grid.imagesLoaded().progress(function () {
-    grid.masonry('layout');
+  var grid = $portfolio_sidebar_grid.imagesLoaded(function () {
+    grid.masonry({
+      columnWidth: '.grid-sizer',
+      gutter: 16,
+      itemSelector: '.grid-item',
+      percentPosition: true
+    });
     $portfolio_sidebar_grid.addClass('loaded');
   });
   jQuery('.masonry img').on('click', function () {
--- js/masonry/masonry-init.js
+++ js/masonry/masonry-init.js
@@ -1,15 +1,13 @@
 jQuery(function() {
   var $portfolio_sidebar_grid = jQuery('.page-template-portfolio-sidebar.page-child .masonry');
 
-  var grid = $portfolio_sidebar_grid.masonry({
-    columnWidth: '.grid-sizer',
-    gutter: 16,
-    itemSelector: '.grid-item',
-    percentPosition: true
-  });
-
-  grid.imagesLoaded().progress(function() { 
-    grid.masonry('layout');
+  var grid = $portfolio_sidebar_grid.imagesLoaded(function() {
+    grid.masonry({
+      columnWidth: '.grid-sizer',
+      gutter: 16,
+      itemSelector: '.grid-item',
+      percentPosition: true
+    });
     $portfolio_sidebar_grid.addClass('loaded');
   });
 
