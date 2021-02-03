eaf69f0a4dddbe2953b7aa4a4e98b5fedb0f8b25
Matt McLean <9052437+doctor-spaceman@users.noreply.github.com>
2021-02-03 12:29:04 -0500

    Portfolio item slider changes include display option for portrait format images, and addition of item title and description to the template if present

--- partials/content-group-gallery.php
+++ partials/content-group-gallery.php
@@ -8,7 +8,10 @@ $gallery_name = get_sub_field('group_name');
 $gallery_desc = get_sub_field('group_description');
 ?>
 
-<ul class="slider slider-<?php echo esc_attr($content_group); ?>">
+<ul class="
+  slider slider-<?php echo esc_attr($content_group); ?> 
+  content-group-<?php echo esc_attr($content_group); ?>
+">
 
 <?php
 if ( have_rows('gallery_images') ) : 
@@ -19,6 +22,7 @@ if ( have_rows('gallery_images') ) :
     $item_desc = get_sub_field('gallery_item_description');
     $item_iframe = get_sub_field('gallery_item_iframe');
     $item_image = get_sub_field('gallery_item');
+    $item_image_is_vertical = get_sub_field('is_vertical');
     $item_image_src = wp_get_attachment_image_url($item_image, 'large');
     $item_image_srcset = wp_get_attachment_image_srcset($item_image, 'large');
     $item_image_sizes = '(min-width: 782px) 50vw,
@@ -27,18 +31,30 @@ if ( have_rows('gallery_images') ) :
     $item_image_alt = get_post_meta( $item_image, '_wp_attachment_image_alt', true );
 ?>
 
-  <li>
+  <li class="<?php if ( $item_name ) : ?> corner--slant<?php endif; ?>">
+  <?php if ( $item_name ) : ?>
+    <h2 class="item-title"><?php echo esc_html($item_name); ?></h2>
+  <?php endif; ?>
+  <?php if ( $gallery_desc ) : ?>
+    <p><?php echo esc_html($gallery_desc); ?></p>
+  <?php endif; ?>
   <?php if ( $item_image ) : ?>
     <img 
-    src="<?php echo $item_image_src; ?>" 
+    <?php if ( $item_image_is_vertical ) : ?>
+    class="is-vertical"
+    <?php endif; ?> 
+    src="<?php echo esc_url($item_image_src); ?>" 
     srcset="<?php echo esc_attr($item_image_srcset); ?>" 
     sizes="<?php echo esc_attr($item_image_sizes); ?>" 
-    alt="<?php echo $item_image_alt; ?>" 
+    alt="<?php echo esc_attr($item_image_alt); ?>" 
     />
   <?php elseif ( $item_iframe ) : ?>
-    <div class="slide-iframe">
+    <div class="responsive-iframe">
       <?php echo $item_iframe; ?>
     </div>
+  <?php endif; ?>
+  <?php if ( $item_desc ) : ?>
+    <p class="supplemental"><?php echo esc_html($item_desc); ?></p>
   <?php endif; ?>
   </li>
 
