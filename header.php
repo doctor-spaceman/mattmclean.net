1ea9c6bc171e24e4ed76fa32ff88b8560f63c89a
Matt McLean <9052437+doctor-spaceman@users.noreply.github.com>
2021-03-16 13:33:16 -0400

    Exclude IE11 from dark mode

--- header.php
+++ header.php
@@ -31,12 +31,6 @@ class="has-sidebar"
 		<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
 		<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
 
-    <!-- TODO -->
-    <noscript>
-      <style>
-      </style>
-    </noscript>
-
 		<?php wp_head(); ?>
 
   </head>
