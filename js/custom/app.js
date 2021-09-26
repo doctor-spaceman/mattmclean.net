ea42870490276e307836557fbc1b56b4225b05b1
Matt McLean <9052437+doctor-spaceman@users.noreply.github.com>
2021-09-26 16:52:46 -0400

    Remove masonry library and simplify the content grid display for the portfolio sidebar template

--- js/custom/app.js
+++ js/custom/app.js
@@ -77,7 +77,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
       });
     }
   };
-  
 
   /*------ Content Overlay ------*/
   const overlay = document.querySelector('.overlay');
@@ -104,4 +103,10 @@ window.addEventListener('DOMContentLoaded', (event) => {
       overlayContents.innerHTML = '';
     };
   }
+
+  /*------ Portfolio Sidebar Image Grid Load ------*/
+  var portfolio_sidebar_grid = document.querySelector('.page-template-portfolio-sidebar.page-child .masonry');
+  imagesLoaded(portfolio_sidebar_grid, function() {
+    portfolio_sidebar_grid.classList.add('loaded');
+  });
 });
