e05043e607300425c5fd6ca021323714b6b5fa35
Matt McLean <9052437+doctor-spaceman@users.noreply.github.com>
2021-09-28 11:11:09 -0400

    Replicate previous portfolio page slider functionality with Swiper

--- js/slider/slider.js
+++ js/slider/slider.js
@@ -1,27 +1,66 @@
 window.addEventListener('DOMContentLoaded', () => {
   const leftSlider = document.querySelector('.slider-vertical__left');
   const leftSlides = leftSlider.querySelectorAll('.swiper-slide');
-  let slidesToShow = 1;
+  let slidesToShow = 3;
   let centerMode = true;
 
   if ( leftSlides.length < 3 ) {
     centerMode = false;
   } else {
-    slidesToShow = 3;
+    slidesToShow = 7;
   }
 
+  // Left-side slider
   const leftSwiper = new Swiper(leftSlider, {
     centeredSlides: true,
-    direction: 'vertical',
+    direction: 'horizontal',
     loop: true,
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-    slidesPerView: 7,
+    slidesPerView: 1,
+    slidesPerGroup: 1, 
     spaceBetween: 16,
+    watchOverflow: true,
+    breakpoints: {
+      599: {
+        centeredSlides: true,
+        direction: 'horizontal',
+        slidesPerView: 3,
+      },
+      782: {
+        centeredSlides: centerMode,
+        direction: 'vertical',
+        slidesPerView: slidesToShow,
+      },
+    }
   });
 
+  // Right-side slider
+  let rightSwiper;
+  let rightSideOptions = {
+    autoHeight: false,
+    centeredSlides: false,
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+    loop: true,
+    slidesPerView: 1,
+  };
+
+  rightSwiper = new Swiper(document.querySelector('.slider-vertical__right:not(.adaptive-height)'), rightSideOptions);
+
+  if (document.querySelector('.slider-vertical__right.adaptive-height')) {
+    rightSideOptions.autoHeight = true;
+    rightSwiper = new Swiper(document.querySelector('.slider-vertical__right.adaptive-height'), rightSideOptions);
+  }
+
+  // Control right with left
+  leftSwiper.on('slideChange', function () {
+    rightSwiper.slideToLoop(leftSwiper.realIndex);
+  });
 });
 
 
--- templates/portfolio.php
+++ templates/portfolio.php
@@ -19,7 +19,7 @@
   <div class="slider-vertical grid">
     <div class="col-1-2">
       <div class="swiper slider-vertical__left skew--left">
-        <div class="swiper-button-prev"></div>
+        <button type="button" class="swiper-button-prev" aria-label="Previous Item"><svg aria-hidden="true" class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7"></path></svg></button>
         <div class="swiper-wrapper">
         <?php 
         // Query the children of this page
@@ -39,32 +39,34 @@
         endif; 
         ?>
         </div>
-        <div class="swiper-button-next"></div>
+        <button type="button" class="swiper-button-next" aria-label="Next Item"><svg aria-hidden="true" class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>        
       </div>
     </div>
     <div class="col-1-2">
-      <div id="walkway" class="slider slider-vertical__right">
-      <?php 
-      if ( $child_pages->have_posts() ) : while ( $child_pages->have_posts() ) : $child_pages->the_post(); 
-        $section_icon = get_field('section_icon'); ?>
-        
-        <div>
-
+      <div id="walkway" class="swiper swiper-no-swiping slider-vertical__right">
+        <div class="swiper-wrapper">
         <?php 
-        if ( $section_icon ) : 
-          $section_icon_url = $section_icon['url'];
-          echo file_get_contents($section_icon_url); 
-        endif; ?>
-        
-          <div class="section">
-            <p><?php echo get_the_excerpt(); ?></p>
-            <a class="cta" href="<?php the_permalink(); ?>">View <?php the_title(); ?></a>
+        if ( $child_pages->have_posts() ) : while ( $child_pages->have_posts() ) : $child_pages->the_post(); 
+          $section_icon = get_field('section_icon'); ?>
+          
+          <div class="swiper-slide">
+
+          <?php 
+          if ( $section_icon ) : 
+            $section_icon_url = $section_icon['url'];
+            echo file_get_contents($section_icon_url); 
+          endif; ?>
+          
+            <div class="section">
+              <p><?php echo get_the_excerpt(); ?></p>
+              <a class="cta" href="<?php the_permalink(); ?>">View <?php the_title(); ?></a>
+            </div>
           </div>
+        <?php 
+        endwhile;
+        endif; 
+        ?>
         </div>
-      <?php 
-      endwhile;
-      endif; 
-      ?>
       </div>  
     </div>
   </div>
